d60502554fc84095070d4fb0d6c6843abcd2b551
bruna-raisa <bruna.raissa2003@gmail.com>
2022-03-18 16:58:52 -0300

    meu ultimo commit

---
+++ letras/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace letras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.chkpe_peteca = new System.Windows.Forms.CheckBox();
+            this.chkca_peteca = new System.Windows.Forms.CheckBox();
+            this.chkte_peteca = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.chkpa_pato = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.chkpi_pipoca = new System.Windows.Forms.CheckBox();
+            this.chkto_pato = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.chkpo_pipoca = new System.Windows.Forms.CheckBox();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
+            this.chkca_pipoca = new System.Windows.Forms.CheckBox();
+            this.checkBox16 = new System.Windows.Forms.CheckBox();
+            this.checkBox17 = new System.Windows.Forms.CheckBox();
+            this.checkBox18 = new System.Windows.Forms.CheckBox();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.btnchecar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(50, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(303, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(165, 122);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(550, 81);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(154, 122);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Clicando Nas Sílabas";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(76, 221);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(34, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "pi";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // chkpe_peteca
+            // 
+            this.chkpe_peteca.AutoSize = true;
+            this.chkpe_peteca.Location = new System.Drawing.Point(76, 244);
+            this.chkpe_peteca.Name = "chkpe_peteca";
+            this.chkpe_peteca.Size = new System.Drawing.Size(38, 17);
+            this.chkpe_peteca.TabIndex = 5;
+            this.chkpe_peteca.Text = "pe";
+            this.chkpe_peteca.UseVisualStyleBackColor = true;
+            // 
+            // chkca_peteca
+            // 
+            this.chkca_peteca.AutoSize = true;
+            this.chkca_peteca.Location = new System.Drawing.Point(133, 244);
+            this.chkca_peteca.Name = "chkca_peteca";
+            this.chkca_peteca.Size = new System.Drawing.Size(38, 17);
+            this.chkca_peteca.TabIndex = 6;
+            this.chkca_peteca.Text = "ca";
+            this.chkca_peteca.UseVisualStyleBackColor = true;
+            // 
+            // chkte_peteca
+            // 
+            this.chkte_peteca.AutoSize = true;
+            this.chkte_peteca.Location = new System.Drawing.Point(133, 221);
+            this.chkte_peteca.Name = "chkte_peteca";
+            this.chkte_peteca.Size = new System.Drawing.Size(35, 17);
+            this.chkte_peteca.TabIndex = 7;
+            this.chkte_peteca.Text = "te";
+            this.chkte_peteca.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(194, 221);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(31, 17);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "ti";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(190, 244);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(35, 17);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "ta";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // chkpa_pato
+            // 
+            this.chkpa_pato.AutoSize = true;
+            this.chkpa_pato.Location = new System.Drawing.Point(303, 221);
+            this.chkpa_pato.Name = "chkpa_pato";
+            this.chkpa_pato.Size = new System.Drawing.Size(38, 17);
+            this.chkpa_pato.TabIndex = 10;
+            this.chkpa_pato.Text = "pa";
+            this.chkpa_pato.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(303, 247);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(31, 17);
+            this.checkBox8.TabIndex = 11;
+            this.checkBox8.Text = "ti";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(368, 221);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(35, 17);
+            this.checkBox9.TabIndex = 12;
+            this.checkBox9.Text = "te";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // chkpi_pipoca
+            // 
+            this.chkpi_pipoca.AutoSize = true;
+            this.chkpi_pipoca.Location = new System.Drawing.Point(550, 221);
+            this.chkpi_pipoca.Name = "chkpi_pipoca";
+            this.chkpi_pipoca.Size = new System.Drawing.Size(34, 17);
+            this.chkpi_pipoca.TabIndex = 13;
+            this.chkpi_pipoca.Text = "pi";
+            this.chkpi_pipoca.UseVisualStyleBackColor = true;
+            // 
+            // chkto_pato
+            // 
+            this.chkto_pato.AutoSize = true;
+            this.chkto_pato.Location = new System.Drawing.Point(368, 244);
+            this.chkto_pato.Name = "chkto_pato";
+            this.chkto_pato.Size = new System.Drawing.Size(35, 17);
+            this.chkto_pato.TabIndex = 14;
+            this.chkto_pato.Text = "to";
+            this.chkto_pato.UseVisualStyleBackColor = true;
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(550, 247);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(38, 17);
+            this.checkBox12.TabIndex = 15;
+            this.checkBox12.Text = "pa";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // chkpo_pipoca
+            // 
+            this.chkpo_pipoca.AutoSize = true;
+            this.chkpo_pipoca.Location = new System.Drawing.Point(611, 221);
+            this.chkpo_pipoca.Name = "chkpo_pipoca";
+            this.chkpo_pipoca.Size = new System.Drawing.Size(38, 17);
+            this.chkpo_pipoca.TabIndex = 16;
+            this.chkpo_pipoca.Text = "po";
+            this.chkpo_pipoca.UseVisualStyleBackColor = true;
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.AutoSize = true;
+            this.checkBox14.Location = new System.Drawing.Point(611, 247);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Size = new System.Drawing.Size(38, 17);
+            this.checkBox14.TabIndex = 17;
+            this.checkBox14.Text = "ce";
+            this.checkBox14.UseVisualStyleBackColor = true;
+            // 
+            // chkca_pipoca
+            // 
+            this.chkca_pipoca.AutoSize = true;
+            this.chkca_pipoca.Location = new System.Drawing.Point(666, 221);
+            this.chkca_pipoca.Name = "chkca_pipoca";
+            this.chkca_pipoca.Size = new System.Drawing.Size(38, 17);
+            this.chkca_pipoca.TabIndex = 18;
+            this.chkca_pipoca.Text = "ca";
+            this.chkca_pipoca.UseVisualStyleBackColor = true;
+            // 
+            // checkBox16
+            // 
+            this.checkBox16.AutoSize = true;
+            this.checkBox16.Location = new System.Drawing.Point(666, 247);
+            this.checkBox16.Name = "checkBox16";
+            this.checkBox16.Size = new System.Drawing.Size(38, 17);
+            this.checkBox16.TabIndex = 19;
+            this.checkBox16.Text = "pe";
+            this.checkBox16.UseVisualStyleBackColor = true;
+            // 
+            // checkBox17
+            // 
+            this.checkBox17.AutoSize = true;
+            this.checkBox17.Location = new System.Drawing.Point(430, 221);
+            this.checkBox17.Name = "checkBox17";
+            this.checkBox17.Size = new System.Drawing.Size(38, 17);
+            this.checkBox17.TabIndex = 20;
+            this.checkBox17.Text = "pe";
+            this.checkBox17.UseVisualStyleBackColor = true;
+            // 
+            // checkBox18
+            // 
+            this.checkBox18.AutoSize = true;
+            this.checkBox18.Location = new System.Drawing.Point(430, 244);
+            this.checkBox18.Name = "checkBox18";
+            this.checkBox18.Size = new System.Drawing.Size(38, 17);
+            this.checkBox18.TabIndex = 21;
+            this.checkBox18.Text = "po";
+            this.checkBox18.UseVisualStyleBackColor = true;
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(513, 335);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 22;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            // 
+            // btnchecar
+            // 
+            this.btnchecar.Location = new System.Drawing.Point(629, 335);
+            this.btnchecar.Name = "btnchecar";
+            this.btnchecar.Size = new System.Drawing.Size(75, 23);
+            this.btnchecar.TabIndex = 23;
+            this.btnchecar.Text = "Checar";
+            this.btnchecar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnchecar);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.checkBox18);
+            this.Controls.Add(this.checkBox17);
+            this.Controls.Add(this.checkBox16);
+            this.Controls.Add(this.chkca_pipoca);
+            this.Controls.Add(this.checkBox14);
+            this.Controls.Add(this.chkpo_pipoca);
+            this.Controls.Add(this.checkBox12);
+            this.Controls.Add(this.chkto_pato);
+            this.Controls.Add(this.chkpi_pipoca);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.chkpa_pato);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.chkte_peteca);
+            this.Controls.Add(this.chkca_peteca);
+            this.Controls.Add(this.chkpe_peteca);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox chkpe_peteca;
+        private System.Windows.Forms.CheckBox chkca_peteca;
+        private System.Windows.Forms.CheckBox chkte_peteca;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox chkpa_pato;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox chkpi_pipoca;
+        private System.Windows.Forms.CheckBox chkto_pato;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.CheckBox chkpo_pipoca;
+        private System.Windows.Forms.CheckBox checkBox14;
+        private System.Windows.Forms.CheckBox chkca_pipoca;
+        private System.Windows.Forms.CheckBox checkBox16;
+        private System.Windows.Forms.CheckBox checkBox17;
+        private System.Windows.Forms.CheckBox checkBox18;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Button btnchecar;
+    }
+}
+
